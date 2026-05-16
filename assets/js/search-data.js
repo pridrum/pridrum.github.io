bb6b1ef0fc574f58f98e005f970e421379e40ffa
pridrum <31519905+pridrum@users.noreply.github.com>
2026-05-16 02:37:52 +0000

    Deploying to gh-pages from @ pridrum/pridrum.github.io@3faf7ebc919bc57f57df7957d6af89ec764891a6 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -511,21 +511,14 @@ ninja.data = [{
         title: 'CV',
         section: 'Socials',
         handler: () => {
-          window.open("/assets/pdf/example_pdf.pdf", "_blank");
+          window.open("/assets/pdf/drummond_pdf.pdf", "_blank");
         },
       },{
         id: 'social-email',
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
-        },
-      },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("mailto:%70%72%69%73%63%69%6C%61%61%64%72%75%6D%6D%6F%6E%64@%67%6D%61%69%6C.%63%6F%6D", "_blank");
         },
       },{
         id: 'social-rss',
@@ -539,14 +532,14 @@ ninja.data = [{
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
+          window.open("https://scholar.google.com/citations?user=RuQheT8AAAAJ", "_blank");
         },
       },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
+        id: 'social-orcid',
+        title: 'ORCID',
         section: 'Socials',
         handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://orcid.org/0000-0002-0028-271X", "_blank");
         },
       },{
       id: 'light-theme',
